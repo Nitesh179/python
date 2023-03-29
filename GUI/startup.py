38cff013d0541d1a0109105d9af353f5a0bcf36d
Nitesh bagora <bnitesh179@gmail.com>
2023-03-29 19:54:31 +0530

    design code

--- GUI/startup.py
+++ GUI/startup.py
@@ -1,2 +1,17 @@
 from tkinter import *
+import json
 
+
+class Startup:
+    def __init__(self):
+        f=open('startups.json','r')
+        self.data= json.load(f)
+        # print(self.data)
+        self.loadGui()
+
+    def loadGui(self):
+        root=Tk()
+        
+
+
+obj=Startup()
